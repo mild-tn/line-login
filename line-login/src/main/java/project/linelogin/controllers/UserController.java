399402755ec409e3eb3ecd56d7e0027743c5c155
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-12-23 13:52:41 +0700

    [Update] add route for registrant

--- line-login/src/main/java/project/linelogin/controllers/UserController.java
+++ line-login/src/main/java/project/linelogin/controllers/UserController.java
@@ -1,17 +1,38 @@
 package project.linelogin.controllers;
 
-import project.linelogin.models.UserModel;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import project.linelogin.repositories.UserRepository;
+import project.linelogin.services.UserService;
 
 
 @RestController
 @CrossOrigin("*")
 public class UserController {
 
+    @Autowired
+    UserService userService;
+
+    @Qualifier("userRepository")
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping("/user")
-    public String getUser() {
-        UserModel userModel = new UserModel("mild","908800",78086679);
-        return userModel.getUsername() + userModel.getPassword();
+    public Object getUser() {
+        return userService.getUserByUsername("mildTN");
+    }
+
+    @PostMapping("/registrant")
+    public String registrant() {
+        return "Login";
+    }
+
+    @PostMapping("/login")
+    public String login() {
+        return "Login";
     }
 }
