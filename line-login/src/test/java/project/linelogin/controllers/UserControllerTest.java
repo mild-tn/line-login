62130ede2e14a1a911bbfef29b175462dd5fbb8c
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-12-23 13:51:14 +0700

    [Add] test UserControllerTest.java

---
+++ line-login/src/test/java/project/linelogin/controllers/UserControllerTest.java
@@ -0,0 +1,9 @@
+package project.linelogin.controllers;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+public class UserControllerTest {
+
+}
