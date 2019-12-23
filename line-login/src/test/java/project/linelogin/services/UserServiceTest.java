73c6773cf689431a347202c7aa78bb0d6ebf2d4c
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-12-23 13:51:38 +0700

    [Add] test UserServiceTest.java

---
+++ line-login/src/test/java/project/linelogin/services/UserServiceTest.java
@@ -0,0 +1,8 @@
+package project.linelogin.services;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserServiceTest {
+}
