b2cad8af4543d24c3c5a5dfb542234bb337d2899
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-12-24 09:13:19 +0700

    remove all app

--- src/test/java/project/linelogin/services/UserServiceTest.java
+++
@@ -1,8 +0,0 @@
-package project.linelogin.services;
-
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
-
-@RunWith(MockitoJUnitRunner.class)
-public class UserServiceTest {
-}
