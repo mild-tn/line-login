1493fd8e2b93b6d885b1fcf81ef50c4d42b047fe
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-12-24 09:18:31 +0700

    Add new line login

---
+++ src/test/java/project/linelogin/LineLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package project.linelogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
